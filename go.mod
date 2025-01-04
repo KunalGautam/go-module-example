bb564ea0f92ff4112f5c464489aa69ee32809828
Kunal Gautam <kunal@ikunal.in>
2025-01-04 15:19:50 +0530

    Test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/KunalGautam/go-module-example
+
+go 1.23.4
---
+++ say/main.go
@@ -0,0 +1,13 @@
+package say
+
+import (
+	"fmt"
+)
+
+func Hello() {
+	fmt.Println("Hello, World! I'm from Package")
+}
+
+func main() {
+	Hello()
+}
